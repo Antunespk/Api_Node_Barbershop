69d805a034d8d7d46138d83d32d4954269e9ad09
antunespk <antunes.patrick@hotmail.com>
2023-01-30 00:14:01 -0300

    api funcionando

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.status(200).json({ msg: "Olá mundo!" })
 });
-app.listen(5000);
-app.use(cors());
+app.listen(3000);
+// app.use(cors());
 
 //Configurando Express
 app.use(express.urlencoded({ extended: true }));
@@ -24,21 +24,23 @@ app.get("/", auth.checkToken, function (req, res) {
     res.status(200).json({ message: "Bem vindo!" });
 });
 
-const userRouter = require("./routes/userRouter");
+// const userRouter = require("./routes/userRouter");
 
-app.use(userRouter);
+// app.use(userRouter);
 
-//Banco de Dados
-// const DB_USER = process.env.DB_USER;
-// const DB_PASS = encodeURIComponent(process.env.DB_PASS);
-// const DB_URI = mongodb + srv://ZERO21:<barbershop021>@api021.h2x5dw1.mongodb.net/test;
+// //Banco de Dados
+// // const DB_USER = process.env.DB_USER;
+// // const DB_PASS = encodeURIComponent(process.env.DB_PASS);
+// // const DB_URI = 'mongodb + srv://:<barbershop021>@api021.h2x5dw1.mongodb.net/test';
 
-// mongoose.set('strictQuery', false);
-// mongoose.connect(process.env.URI)
-//     .then(result => {
-//         console.log("Conectado!");
-//         app.listen(process.env.PORT);
-//     })
-//     .catch(err => {
-//         console.error("Error: ", err.message);
-//     });
+// Para arquivo .env: DB_USER:ZERO21 DB_PASS:barbershop021
+
+// // mongoose.set('strictQuery', false);
+// // mongoose.connect(process.env.URI)
+// //     .then(result => {
+// //         console.log("Conectado!");
+// //         app.listen(process.env.PORT);
+// //     })
+// //     .catch(err => {
+// //         console.error("Error: ", err.message);
+// //     });
